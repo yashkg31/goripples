7a0fb7c391d3ca6490a5d1d47a37b11c7839c966
Yash Gupta <109368749+yashkg31@users.noreply.github.com>
2024-08-23 00:07:53 +0530

    added pagination

--- src/components/RewardsList.jsx
+++ src/components/RewardsList.jsx
@@ -3,11 +3,15 @@ import { useRecoilValue } from 'recoil';
 import { dashboardState } from '../recoil/atoms';
 import RewardItem from './RewardItem';
 import { BiSearch } from "react-icons/bi";
+import { GrNext } from "react-icons/gr";
+import { GrPrevious } from "react-icons/gr";
+
 
 const RewardsList = () => {
   const rewards = useRecoilValue(dashboardState);
   const [sortBy, setSortBy] = useState('date');
   const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(1);
 
   const filteredRewards = rewards
     .filter((reward) => reward.brand.toLowerCase().includes(searchTerm.toLowerCase()))
@@ -17,6 +21,21 @@ const RewardsList = () => {
       }
       return new Date(b.purchaseDate) - new Date(a.purchaseDate);
     });
+
+    const totalItems = rewards.length;
+    
+    const totalPages = parseInt(totalItems/20 + 1);
+    
+
+    const handlePrevClick = () => {
+      if(page >= 2)
+        setPage(prev => prev - 1)
+    }
+
+    const handleNextClick = () => {
+      if(page < totalPages)
+        setPage(prev => prev + 1)
+    }
 
   return (
     <div className='pt-40 mx-10 mb-5'>
@@ -41,10 +60,16 @@ const RewardsList = () => {
         </select>
       </div>
       <ul className="grid grid-cols-2 gap-y-6 gap-x-9">
-        {filteredRewards.map((reward) => (
+        {filteredRewards.slice((page - 1) * 20, page * 20).map((reward) => (
           <RewardItem key={reward.id} reward={reward} />
         ))}
       </ul>
+
+      <div className='flex justify-center gap-3 mt-8 mb-4 text-black text-2xl font-black'>
+        <button onClick={handlePrevClick}><GrPrevious /></button>
+        <button onClick={handleNextClick}><GrNext /></button>
+      </div>
+
     </div>
   );
 };
