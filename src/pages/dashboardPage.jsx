363b3199069740f969b9373166041b66dcaa63b4
Yash Gupta <109368749+yashkg31@users.noreply.github.com>
2024-08-25 22:57:28 +0530

    .

--- src/pages/dashboardPage.jsx
+++ src/pages/dashboardPage.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Dashboard from '../components/Dashboard';
 import { useRecoilValue } from 'recoil';
 import { userDetails } from '../recoil/atoms';
+import { Link } from 'react-router-dom';
 
 const DashboardPage = () => {
   const user = useRecoilValue(userDetails)
 
   return (
     <div className="">
-      {user.user_data ? <Dashboard /> : <p className='text-black text-5xl'>Login first!</p>}
+      {user.user_data ? <Dashboard /> : <Link className='text-black text-5xl underline hover:text-red-600' to={"/"}>Login first!</Link>}
     </div>
   );
 };
